e11c4f8e77e6de9dfe79f1a97f665506bc358232
Lemonadeccc <zwjhb12@163.com>
2025-01-12 13:43:22 +0800

    fix: 🧩 注掉暂未实现特有插件
    注掉暂未实现特有插件

--- packages/core/src/utils/select.ts
+++ packages/core/src/utils/select.ts
@@ -158,7 +158,7 @@ async function projectSelect() {
     react: [
       { key: "mobx", value: "mobx" },
       { key: "react-router", value: "react-router" },
-      { key: "antd", value: "antd" },
+      // { key: "antd", value: "antd" },
       {
         key: "null",
         value: chalk.greenBright(skip),
@@ -166,8 +166,9 @@ async function projectSelect() {
       },
     ],
     vue: [
-      { key: "vuex", value: "vuex" },
-      { key: "vue-router", value: "vue-router" },
+      // TODO 暂时还没添加
+      // { key: "vuex", value: "vuex" },
+      // { key: "vue-router", value: "vue-router" },
       { key: "element-plus", value: "element-plus" },
       { key: "pinia", value: "pinia" },
       {
